71f0720fab6910dc29fe686ca8c7f5463572a743
Umakajan Umakanthan <umakajan@umakajan.com>
2020-01-12 14:27:25 -0500

    create get messages service

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 
 app.use(express.static(__dirname))
 
-var server = app.listen(3000, () => {
+const messages = [
+    { name: 'Tim', message: 'hi'},
+    { name: 'Jack', message: 'hello'},
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+})
+
+const server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port);
 })
