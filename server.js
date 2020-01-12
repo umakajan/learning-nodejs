8bbee3a0900f11604df9e0a2c0e236dd3f8eadf5
Umakajan Umakanthan <umakajan@umakajan.com>
2020-01-12 14:44:44 -0500

    create post message service

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const messages = [
-    { name: 'Tim', message: 'hi'},
+    { name: 'Tim', message: 'hi!'},
     { name: 'Jack', message: 'hello'},
 ]
 
 app.get('/messages', (req, res) => {
     res.send(messages);
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200);
 })
 
 const server = app.listen(3000, () => {
