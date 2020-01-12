6dd45cbd12db7ccfb1cfe77f68634db8ea6043f5
Umakajan Umakanthan <umakajan@umakajan.com>
2020-01-12 14:15:57 -0500

    create express server, serve static file

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname))
+
+var server = app.listen(3000, () => {
+    console.log('server is listening on port', server.address().port);
+})
